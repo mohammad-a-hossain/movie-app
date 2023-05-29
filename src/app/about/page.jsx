6c83b1cfbe1fd9f4e9034da0031f73ec53924056
mohammad-a-hossain <adam.eve007@gmail.com>
2023-05-29 12:18:31 +0600

    set up about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto'>
+    
+       <h1 className='...'>about page</h1> 
+       <hr/>
+       <p>
+       
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis auctor turpis, ut eleifend dolor hendrerit eget. Aenean vitae commodo lorem, at blandit justo. Morbi dapibus ante vitae velit rutrum lacinia vitae non dui. Maecenas sodales mauris a sem mollis ornare. Donec malesuada tortor sed finibus venenatis. Pellentesque eget enim fermentum, viverra neque sed, dapibus sapien. Phasellus scelerisque eu ipsum vitae ultricies. Nunc ut dolor justo. Pellentesque nec pulvinar nisi, id mollis diam. Aliquam ultricies elit ut orci placerat venenatis. Vivamus arcu nisl, pulvinar in volutpat sit amet, rutrum vitae risus.
+       </p>
+       Duis imperdiet quam sit amet libero lobortis, vel rhoncus sapien volutpat. Nullam sollicitudin nec ante eu bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec porttitor ultricies libero, vitae vulputate orci faucibus quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam egestas lobortis velit sed pulvinar. Nunc ex turpis, mattis at interdum eu, mollis non risus. Proin dictum malesuada luctus. Cras in massa elit. Etiam ut magna blandit, rutrum nisi id, mollis magna. Proin ultrices erat mattis, accumsan lacus vel, eleifend justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+       <p>
+       Aliquam auctor tellus in ex scelerisque tempor. Pellentesque vitae condimentum tortor. Ut scelerisque ipsum arcu, sed consectetur arcu elementum quis. Proin condimentum sed dolor et condimentum. Ut ut convallis risus, sed condimentum nulla. Nulla viverra sapien quis orci scelerisque condimentum. Maecenas mattis iaculis ipsum, in auctor sem posuere sit amet. In malesuada pretium erat, eget tristique magna fermentum dapibus. Suspendisse at nisi dictum, rutrum lorem at, euismod tellus. Sed pharetra, sem quis volutpat porttitor, orci quam volutpat arcu, vel malesuada risus felis vitae diam. Maecenas lacinia hendrerit tempus.
+         </p>
+       Aenean vel varius quam. Cras eu fringilla velit, eget vulputate odio. Cras vitae nulla a orci dignissim imperdiet nec at tortor. Aenean non sollicitudin odio. Vivamus eu mollis tortor. Nunc ac imperdiet risus. Nullam a justo nec justo eleifend luctus sit amet at ipsum. Mauris maximus sit amet ex at hendrerit. Cras porttitor rutrum justo sit amet finibus. Aliquam sed ornare nulla. Etiam sed urna a libero bibendum ultrices. Quisque non mauris vel nisi vestibulum rutrum egestas ut ex. Ut facilisis, nunc sit amet tempor tincidunt, ante nisl laoreet lorem, sed maximus nunc felis non dui. Suspendisse potenti.
+         <p>
+       Maecenas eget metus ac dui facilisis facilisis at vitae nisl. Nam vel turpis gravida, gravida tellus maximus, pretium felis. In eget neque ante. Duis dapibus sed tortor ac facilisis. Suspendisse congue vulputate lorem nec faucibus. Nunc et dapibus erat, ut feugiat nisi. Vestibulum id sodales est, vel tempus metus. Vestibulum in dolor nisi. Mauris eget scelerisque nisl. Nam quis tincidunt nisi, mattis volutpat quam. Aliquam id aliquam libero. Proin aliquam diam augue, non tempor lacus laoreet sit amet.
+       </p>
+    
+    </div>
+  )
+}
